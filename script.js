896d2d13271d9e0e3a23063f1ba17a71092b262b
vidyarthi <vidyarthi004@gmail.com>
2022-11-13 17:27:19 +0530

    latest push

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let url = "https://kontests.net/api/v1/all"
 let response = fetch(url)
 response.then((v) => {
-    return v.json()
+  return v.json()
 }).then((contest) => {
-    console.log(contest)
-    ihtml = ""
-    for (item in contest) {
-        console.log(contest[item])
-        ihtml +=
-            `
+  console.log(contest)
+  ihtml = ""
+  for (item in contest) {
+    console.log(contest[item])
+    ihtml +=
+      `
             <li>
               <div class="course-card">
 
                 <figure class="card-banner">
-                  <img src="./assets/images/course-1.jpg" width="370" height="270" loading="lazy"
+                  <img src="https://images.techopedia.com/images/uploads/istock-1173805290.jpeg?w=800&h=0&mode=max&quality=70&scale=both" width="370" height="270" loading="lazy"
                     alt="Competitive Strategy law for all students" class="img-cover">
                 </figure>
 
                 <div class="card-actions">
 
-                  <span class="badge">Intermediate</span>
+                  <span class="badge">${contest[item].site}</span>
 
                   <button class="whishlist-btn" aria-label="Add to whishlist" data-whish-btn>
                     <ion-icon name="heart"></ion-icon>
@@ -28,53 +28,12 @@ response.then((v) => {
                 </div>
 
                 <div class="card-content">
-
-                  <ul class="card-meta-list">
-
-                    <li class="card-meta-item">
-                      <ion-icon name="reader-outline" aria-hidden="true"></ion-icon>
-
-                      <span class="card-meta-text">35 Lessons</span>
-                    </li>
-
-                    <li class="card-meta-item">
-                      <ion-icon name="time-outline" aria-hidden="true"></ion-icon>
-
-                      <time datetime="PT18H15M44S" class="card-meta-text">18h 15m 44s</time>
-                    </li>
-
-                  </ul>
-
                   <h3 class="h3">
-                    <a href="#" class="card-title">Competitive Strategy law for all students</a>
+                    <a href="#" class="card-title">${contest[item].name}</a>
                   </h3>
-
-                  <div class="rating-wrapper">
-
-                    <div class="rating">
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                    </div>
-
-                    <span class="rating-text">(18 Review)</span>
-
-                  </div>
-
                   <div class="card-footer">
-
-                    <div class="card-price">
-                      <span class="span">$29.00</span>
-
-                      <del class="del">$39.00</del>
-                    </div>
-
                     <div class="card-meta-item">
-                      <ion-icon name="people-outline" aria-hidden="true"></ion-icon>
-
-                      <span class="card-meta-text">47 Students</span>
+                      <a href="${contest[item].url}" class="card-meta-text">View Details</a>
                     </div>
 
                   </div>
@@ -84,6 +43,6 @@ response.then((v) => {
               </div>
             </li>
         `
-    }
-    cardContainer.innerHTML = ihtml
+  }
+  cardContainer.innerHTML = ihtml
 })
